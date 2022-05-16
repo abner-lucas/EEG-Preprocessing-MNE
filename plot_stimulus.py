508a96417bb37779934f901b4fa2b353e4407ca7
Abner Lucas <abnerlucas.cad@gmail.com>
2022-05-16 17:13:59 -0300

    Analyzing events/stimulus

--- plot_stimulus.py
+++ plot_stimulus.py
@@ -9,11 +9,5 @@ raw_data = mne.io.read_raw_brainvision(file, preload=True)
 
 events_stimulus, event_stimulus_dict = mne.events_from_annotations(raw_data)
 
-#save events_stimulus em txt
-with open(f'datasets/events_stimulus_{name_file}.txt', 'w') as f:
-    for item in events_stimulus:
-        f.write("%s\n" % item)
-
-#plot somente 30 events_stimulus
 mne.viz.plot_events(events_stimulus, event_id=event_stimulus_dict, sfreq=raw_data.info['sfreq'])
 plt.show()

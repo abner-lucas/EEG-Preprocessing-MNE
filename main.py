1dfe43ec89c474d18bb056c07269fa297889293f
Abner Lucas <abnerlucas.cad@gmail.com>
2022-05-21 01:20:46 -0300

    Execute in colab

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import os
 import src.load_eeg as ld
 import src.preprocessing_eeg as ppeeg
 
-#path = f'/content/drive/MyDrive/Colab Notebooks/PPGCC/EEG/data_brainvision/'
-path_eeg = f'datasets/data_brainvision/'
+path_eeg = f'/content/drive/MyDrive/Colab Notebooks/PPGCC/EEG/data_brainvision/'
+#path_eeg = f'datasets/data_brainvision/'
 subjects = [f[:-5] for f in os.listdir(path_eeg) if f.endswith('.vhdr')]
-path_events_dic = f'datasets/event_stimulus_new_dict.json'
+path_events_dic = f'/content/preprocessing_EEG_with_MNE/datasets/event_stimulus_new_dict.json'
+#path_events_dic = f'datasets/event_stimulus_new_dict.json'
 
 #raise SystemExit
 
@@ -21,8 +22,8 @@ for sub in subjects:
         df_eeg = pproc.get_data_frame()
         df_eeg = df_eeg.rename(columns={"epoch":"trail"})
 
-        #df_eeg.to_csv('/content/preprocessing_EEG_with_MNE/datasets/' + subject + '_epochs_clean.csv')
-        df_eeg.to_csv('outputs_eeg/' + sub + '.csv')
+        df_eeg.to_csv('/content/preprocessing_EEG_with_MNE/outputs_eeg/' + sub + '.csv')
+        #df_eeg.to_csv('outputs_eeg/' + sub + '.csv')
     
     print('\n')
     print(f'{sub} done!')
